ce6bb652c3b6e5194c7e37222d1f1617d2f40479
Christa Stephens <christastephens@quickenloans.com>
2019-08-19 16:39:01 -0400

    added vars

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PowersTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Friend! Welcome to the Table Of Powers");
+            Console.WriteLine("Please enter a number:");
+            int number = int.Parse(Console.ReadLine());
+            int squared = number * number;
+            int cubed = number * number * number;
+            Console.WriteLine(number + squared + cubed);
+        }
+    }
+}
